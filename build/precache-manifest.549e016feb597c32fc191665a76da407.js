d46969fa007542a83fa1572a29feb63fa89eec94
gunjan <gunjant@webgility.com>
2019-08-01 12:18:39 +0530

    after npm run build moved content to gh-pages

---
+++ build/precache-manifest.549e016feb597c32fc191665a76da407.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61d76592972c66f00aac4b5bfc94e8c",
+    "url": "/ui-react-ob/index.html"
+  },
+  {
+    "revision": "383c916bf68033c0fa6d",
+    "url": "/ui-react-ob/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "643f1857e35f967efa32",
+    "url": "/ui-react-ob/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "383c916bf68033c0fa6d",
+    "url": "/ui-react-ob/static/js/2.6f6378ba.chunk.js"
+  },
+  {
+    "revision": "643f1857e35f967efa32",
+    "url": "/ui-react-ob/static/js/main.a46739e6.chunk.js"
+  },
+  {
+    "revision": "089d58671a24efd9be80",
+    "url": "/ui-react-ob/static/js/runtime~main.66743cf4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ui-react-ob/static/media/logo.5d5d9eef.svg"
+  }
+]);
